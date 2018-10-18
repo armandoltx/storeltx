8c684a8db9cba3bf65e101e7df16481e3777b3de
Armando Ruiz <armando.ruiz@industrie.co>
2018-10-18 11:17:51 +1100

    created Product model

---
+++ db/migrate/20181018001701_create_products.rb
@@ -0,0 +1,17 @@
+class CreateProducts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.string :brand
+      t.string :model
+      t.decimal :price
+      t.string :size
+      t.string :colour
+      t.references :category, foreign_key: true
+      t.references :line_item, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
