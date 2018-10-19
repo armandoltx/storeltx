8527c6c2a087e832f1635aa988887cd69c7c5e27
armandoltx <armando@armandoltx.com>
2018-10-19 16:56:52 +1100

    created all actions for Category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,19 +1,55 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only [:show, :edit, :update, :destroy]
+
   def index
+    @categories ||= Category.all
   end
 
   def show
   end
 
+  def new
+    @category ||= Category.new(category_params)
+  end
+
   def create
+    @category ||= Category.new(category_params)
+
+    if @category.save
+      flash[:notice] = 'Category Successfully created'
+      redirect_to categories_path
+    else
+      flash[:notice] = 'Category is not created'
+      render: :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @category.update(category_params)
+      flash[:notice] = 'Category Successfully created'
+      redirect_to categories_path
+    else
+      flash[:notice] = 'Category is not created'
+      render: :edit
+    end
   end
 
-  def delete
+  def destroy
+    @category.destroy
+    flash[:notice] = 'Category Deleted'
+    redirect_to categories_path
+  end
+
+  private
+
+  def set_category
+    @category || = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name, :description)
   end
 end
