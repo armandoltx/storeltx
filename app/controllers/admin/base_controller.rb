78dd0e9b1f06216645d3a741f8d8f17c817bd909
Armando Ruiz <armando.ruiz@industrie.co>
2018-10-29 09:59:07 +1100

    Created Admin Basse Controller

---
+++ app/controllers/admin/base_controller.rb
@@ -0,0 +1,12 @@
+class Admin::BaseController < ApplicationController
+  before_action :verify_user
+
+  layout "admin"
+
+  private
+
+  def verify_user
+    unless current_user && current_user.admin?
+      redirect_to '/'
+  end
+end
