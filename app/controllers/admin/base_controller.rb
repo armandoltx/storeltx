07269d478b602a240a50d9f45a3ca807cbf56aca
Armando Ruiz <armando.ruiz@industrie.co>
2018-10-29 11:12:51 +1100

    Created admin categories controller

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,12 +1,13 @@
 class Admin::BaseController < ApplicationController
   before_action :verify_user
 
-  layout "admin"
+  # layout "admin"
 
   private
 
   def verify_user
     unless current_user && current_user.admin?
       redirect_to '/'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
 
   devise_for :users, path: '', path_names:{ sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
-  root 'categories#index'
 
-  resources :categories
+  namespace :admin do
+    resources :categories
+
+  end
+
+  resources :categories, only: [:index, :show]
   resources :subcategories
+  root 'categories#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
