5ad6ac1a23892386b681279cb9d3023463582328
Armando Ruiz <armando.ruiz@industrie.co>
2018-11-26 10:35:43 +1100

    wip category controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  before_action: :set_category, only: [:show, :edit, :update, :destroy]
 
   def index
     @categories ||= Category.all
@@ -6,5 +7,35 @@ class CategoriesController < ApplicationController
 
   def show
     @category ||= Category.find(params[:id])
+  end
+
+  def new
+    @category ||= Category.new
+  end
+
+  def create
+    @category ||= Category.new(category_params)
+
+    if@category.save
+      flash[:message] = "Successfully Created #{@category.name}"
+      redirect_to [:admin, @category]
+    else
+      flash[:message] = "Something went wrong and the Category was not created"
+      render action: :new
+    end
+  end
+
+  def edit
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name, :description)
+    
   end
 end
